ffb24438b1fefbc83a2a39d2dc3578ce1137ca28
kkaltenheuser <karenkaltenheuser@gmail.com>
2020-12-05 22:15:47 -0500

    set service worker

---
+++ public/service-worker.js
@@ -0,0 +1,61 @@
+var CACHE_NAME = "my-site-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+// cache these urls
+var urlsToCache = [
+  "/",
+  "./db.js",
+  "./index.js",
+  "./manifest.json",
+  "./styles.css",
+  "./icons/icon-192x192.png",
+  "./icons/icon-512x512.png"
+];
+// install event listener
+self.addEventListener("install", function(event) {
+  // install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+        console.log("Opened cache");
+        // cache these urls
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+// fetch function on event listener
+self.addEventListener("fetch", function(event) {
+  // cache all get requests to api
+  if (event.request.url.includes("/api/")) {
+    event.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(event.request)
+          .then(response => {
+            // If positive response, put in cache
+            if (response.status === 200) {
+              cache.put(event.request.url, response.clone());
+            }
+
+            return response;
+          })
+          .catch(err => {
+            // if error, return/request from cache
+            return cache.match(event.request);
+          });
+      }).catch(err => console.log(err))
+    );
+
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get("accept").includes("text/html")) {
+          // return the cached home page for all requests for html pages
+          return caches.match("/");
+        }
+      });
+    })
+  );
+});
