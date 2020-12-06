db02855e65fb5131d9b4312da266a2df4b9a6c7c
kkaltenheuser <karenkaltenheuser@gmail.com>
2020-12-05 22:20:49 -0500

    populateTable function

--- public/index.js
+++ public/index.js
@@ -22,3 +22,32 @@ fetch("/api/transaction")
     populateTable();
     populateChart();
   });
+
+
+
+// populateTotal
+  function populateTotal() {
+    // reduce transaction amounts to total value
+    let total = transactions.reduce((total, t) => {
+      return total + parseInt(t.value);
+    }, 0);
+  // total query selector
+    let totalEl = document.querySelector("#total");
+    totalEl.textContent = total;
+  }
+  
+  function populateTable() {
+    let tbody = document.querySelector("#tbody");
+    tbody.innerHTML = "";
+  
+    transactions.forEach(transaction => {
+      // create and populate a table row
+      let tr = document.createElement("tr");
+      tr.innerHTML = `
+        <td>${transaction.name}</td>
+        <td>${transaction.value}</td>
+      `;
+  // append the child to table row
+      tbody.appendChild(tr);
+    });
+  }
